6b3690bcc4c4c768ee984060f7d47d4185d3005c
Reshma-evangelin <reshmaevan@gmail.com>
2025-02-14 13:01:24 +0530

    hello

--- App.tsx
+++ App.tsx
@@ -88,7 +88,7 @@ function App(): React.JSX.Element {
           </Section>
           <Section title="Learn More">
             Read the docs to discover what to do next:
-            illa
+            
           </Section>
           <LearnMoreLinks />
         </View>
